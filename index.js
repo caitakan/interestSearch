c4fbe38b0b0952d5cc15c72636f0f9f3b2bbfde6
Jason <v-haimz@microsoft.com>
2018-07-13 01:48:48 -0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "this page is '/'"})
+});
+
+app.get("/api/testhi", (req, res) => {
+  res.send({ hi: "there"});
+});
+
+const PORT = process.env.PORT || 8888;
+app.listen(8888);
