84fcf78430a034e3fc54a6f8a84c50f418cf62b8
Jason <v-haimz@microsoft.com>
2018-07-13 05:57:31 -0700

    temp update config

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '749458406085-3jtf65c6or3v38l5ebsflptlgt7t043c.apps.googleusercontent.com',
+    googleClientSecret: 'EDoEAh-AD3UY0L_a2DiLdLft',
+    mongoURI: 'mongodb://haiming-username:password123@ds018498.mlab.com:18498/haiming-dev',
+    cookieKey: 'wotingjiannideshengyinyouzhongtebiedeganjuerangwobuduanxiangbuganzaiwangjini'
+};
