411ca9b2f0677a7774abd59f21c08032c0d1bb0d
Jason <v-haimz@microsoft.com>
2018-07-13 21:56:22 -0700

    update config

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '749458406085-3jtf65c6or3v38l5ebsflptlgt7t043c.apps.googleusercontent.com',
-    googleClientSecret: 'EDoEAh-AD3UY0L_a2DiLdLft',
-    mongoURI: 'mongodb://haiming-username:password123@ds018498.mlab.com:18498/haiming-dev',
-    cookieKey: 'wotingjiannideshengyinyouzhongtebiedeganjuerangwobuduanxiangbuganzaiwangjini'
-};
+if (process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
