08c30f00dd82607830a5377295d15abb8b9bfef9
Jason <v-haimz@microsoft.com>
2018-07-13 22:27:40 -0700

    temp test prod

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,12 @@
+// module.exports = {
+//     googleClientID: process.env.GOOGLE_CLIENT_ID,
+//     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     mongoURI: process.env.MONGO_URI,
+//     cookieKey: process.env.COOKIE_KEY
+// };
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    googleClientID: '749458406085-3jtf65c6or3v38l5ebsflptlgt7t043c.apps.googleusercontent.com',
+    googleClientSecret: 'EDoEAh-AD3UY0L_a2DiLdLft',
+    mongoURI: 'mongodb://haiming-username:password123@ds018498.mlab.com:18498/haiming-dev',
+    cookieKey: 'wotingjiannideshengyinyouzhongtebiedeganjuerangwobuduanxiangbuganzaiwangjini'
 };
